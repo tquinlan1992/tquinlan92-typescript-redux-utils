ea2e4ab08ed0533650a3568b125632f3bcc47e97
Tom Quinlan <tquinlan1992@gmail.com>
2019-06-02 18:45:25 -0400

    add resolve thunks

--- src/tquinlan92-typescript-redux-utils/react-redux.d.ts
+++ src/tquinlan92-typescript-redux-utils/react-redux.d.ts
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { MapDispatchToPropsParam } from "react-redux";
+import { MapDispatchToPropsParam, ResolveThunks } from "react-redux";
 export interface WithStyles<Styles> {
     classes: {
         [P in keyof Styles]: string;
@@ -7,8 +7,8 @@ export interface WithStyles<Styles> {
 }
 export declare function withStyles<Styles = {}>(styles: Styles): <OtherProps = {}>(Component: React.FunctionComponent<WithStyles<Styles> & OtherProps>) => (props: any) => React.ReactElement<any, string | ((props: any) => React.ReactElement<any, string | any | (new (props: any) => React.Component<any, any, any>)> | null) | (new (props: any) => React.Component<any, any, any>)>;
 
-export declare function createConnectProps<AppState>(): <MapStateToProps extends (state: AppState, ownProps: any) => {}, MapDispatchToProps extends MapDispatchToPropsParam<{}, {}>>(mapStateToProps: MapStateToProps, mapDispatchToProps?: MapDispatchToProps) => (Component: React.FunctionComponent<ReturnType<MapStateToProps> & MapDispatchToProps>) => {
-    Component: React.FunctionComponent<ReturnType<MapStateToProps> & MapDispatchToProps>;
+export declare function createConnectProps<AppState>(): <MapStateToProps extends (state: AppState, ownProps: any) => {}, MapDispatchToProps extends MapDispatchToPropsParam<{}, {}>>(mapStateToProps: MapStateToProps, mapDispatchToProps?: MapDispatchToProps) => (Component: React.FunctionComponent<ReturnType<MapStateToProps> & ResolveThunks<MapDispatchToProps>>) => {
+    Component: React.FunctionComponent<ReturnType<MapStateToProps> & ResolveThunks<MapDispatchToProps>>;
     mapStateToProps: MapStateToProps;
     mapDispatchToProps: MapDispatchToProps;
     Connected: import("react-redux").ConnectedComponentClass<(props: any) => React.ReactElement<any, string | ((props: any) => React.ReactElement<any, string | any | (new (props: any) => React.Component<any, any, any>)> | null) | (new (props: any) => React.Component<any, any, any>)>, Pick<any, never> & Parameters<MapStateToProps>[1]>;
