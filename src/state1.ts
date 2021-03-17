47a33d278facaa7e7d7e2fdff97a728140f0b11f
Tom Quinlan <tquinlan1992@gmail.com>
2021-03-17 01:10:14 -0400

    update package-lock and fix test

--- src/state1.ts
+++ src/state1.ts
@@ -1,20 +1,22 @@
-import { mergeStateWithActions, ThunkActionsForState } from "./tquinlan92-typescript-redux-utils";
+import {
+  mergeStateWithActions,
+  ThunkActionsForState,
+} from "./tquinlan92-typescript-redux-utils";
 import { actions, AppState } from "./store";
 
-
 export const state1ThunkActions: ThunkActionsForState<AppState> = {
-    getResults: () => (dispatch) => {
-        dispatch(actions.state1.results(['item1', 'item2', 'item3']));
-    }
-}
+  getResults: () => (dispatch) => {
+    dispatch(actions.state1.results(["item1", "item2", "item3"]));
+  },
+};
 
 const state1NoActions = {
-    input: '',
-    results: [] as string[]
-}
+  input: "",
+  results: [] as string[],
+};
 
 export const state1 = mergeStateWithActions(state1NoActions, {
-    immuInput: (state, {value}: {value: number}) => {
-        state.input = String(value);
-    }
+  immerInput: (state, { value }: { value: number }) => {
+    state.input = String(value);
+  },
 });
