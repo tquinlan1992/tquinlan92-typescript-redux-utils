9b44e1d8dcb31a982da05b2ff0b03d814c1951f0
Tom Quinlan <tquinlan1992@gmail.com>
2021-03-17 01:44:00 -0400

    update react-scripts

--- src/tquinlan92-typescript-redux-utils/redux.js
+++ src/tquinlan92-typescript-redux-utils/redux.js
@@ -1,175 +1,191 @@
-import { find, mapValues, assign, pick } from 'lodash';
-import actionCreatorFactory, { isType } from "typescript-fsa";
+import { find, mapValues, assign, pick } from "lodash";
+import actionCreatorFactory, { isType } from "../typescript-fsa";
 import produce from "immer";
-import { combineReducers, createStore, applyMiddleware } from 'redux';
+import { combineReducers, createStore, applyMiddleware } from "redux";
 
 export function createReducer(initialState, actions) {
-    return (state = initialState, incomingAction) => {
-        const actionMatch = find(actions, action => {
-            return isType(incomingAction, action.actionCreator);
-        });
-        if (actionMatch) {
-            return actionMatch.reducer(state, incomingAction.payload);
-        } else {
-            return state;
-        }
-    };
+  return (state = initialState, incomingAction) => {
+    const actionMatch = find(actions, (action) => {
+      return isType(incomingAction, action.actionCreator);
+    });
+    if (actionMatch) {
+      return actionMatch.reducer(state, incomingAction.payload);
+    } else {
+      return state;
+    }
+  };
 }
 export function makeActionCreatorWithReducer(name, reducer, withImmer) {
-    const SimpleActionCreator = actionCreatorFactory();
-    return {
-        actionCreator: SimpleActionCreator(name),
-        reducer: (state, action) => {
-            if (withImmer) {
-                return produce(state, draft => {
-                    reducer(draft, action);
-                });
-            } else {
-                return reducer(state, action)
-            }
-        }
-    };
+  const SimpleActionCreator = actionCreatorFactory();
+  return {
+    actionCreator: SimpleActionCreator(name),
+    reducer: (state, action) => {
+      if (withImmer) {
+        return produce(state, (draft) => {
+          reducer(draft, action);
+        });
+      } else {
+        return reducer(state, action);
+      }
+    },
+  };
 }
 
-
 export function getActionCreator() {
-    return (name, reducer) => {
-        return makeActionCreatorWithReducer(name, reducer);
-    }
+  return (name, reducer) => {
+    return makeActionCreatorWithReducer(name, reducer);
+  };
 }
 
 export function getActionCreatorWithImmer() {
-    return (name, reducer) => {
-        return makeActionCreatorWithReducer(name, reducer, true);
-    }
+  return (name, reducer) => {
+    return makeActionCreatorWithReducer(name, reducer, true);
+  };
 }
 
 export function getCreators(creators) {
-    return mapValues(creators, "actionCreator");
+  return mapValues(creators, "actionCreator");
 }
 
 function getActionName(reducerName, actionName) {
-    return JSON.stringify({ reducerName, actionName });
+  return JSON.stringify({ reducerName, actionName });
 }
 
 export function makeActionCreatorWithReducerWithPrefix(actionName, reducer) {
-    return (reducerName) => makeActionCreatorWithReducer(JSON.stringify({ reducerName, actionName }), reducer);
+  return (reducerName) =>
+    makeActionCreatorWithReducer(
+      JSON.stringify({ reducerName, actionName }),
+      reducer
+    );
 }
 
 export function getReducersFromCombinedActionReducer(creators) {
-    return mapValues(creators, "reducer");
+  return mapValues(creators, "reducer");
 }
 
 export function getActionsFromCombinedActionReducer(creators) {
-    return mapValues(creators, "actions");
+  return mapValues(creators, "actions");
 }
 
 export function getActionsAndReducersFromCombinedActionReducer(creators) {
-    return {
-        actions: getActionsFromCombinedActionReducer(creators),
-        reducers: getReducersFromCombinedActionReducer(creators)
-    };
+  return {
+    actions: getActionsFromCombinedActionReducer(creators),
+    reducers: getReducersFromCombinedActionReducer(creators),
+  };
 }
 
 export function testRunner(reducer) {
-    return (initalState, action) => {
-        return reducer(initalState, action);
-    };
+  return (initalState, action) => {
+    return reducer(initalState, action);
+  };
 }
 
 export function makeSimpleReducer(reducerName, initialState, otherActions) {
-    const actions = mapValues(initialState, (propertyFromState, key) => {
-        const creatorReducer = makeActionCreatorWithReducerWithPrefix(
-            `UPDATE_${key}`,
-            (state, newValue) => {
-                return assign({}, state,
-                    { [key]: newValue }
-                );
-            }
-        );
-        return creatorReducer(reducerName);
-    });
+  const actions = mapValues(initialState, (propertyFromState, key) => {
+    const creatorReducer = makeActionCreatorWithReducerWithPrefix(
+      `UPDATE_${key}`,
+      (state, newValue) => {
+        return assign({}, state, { [key]: newValue });
+      }
+    );
+    return creatorReducer(reducerName);
+  });
 
-    const reset = makeActionCreatorWithReducer(getActionName(reducerName, 'Reset'),
-        (state, keysToReset) => {
-            return assign({},
-                state,
-                pick(initialState, keysToReset)
-            );
-        }
+  const reset = makeActionCreatorWithReducer(
+    getActionName(reducerName, "Reset"),
+    (state, keysToReset) => {
+      return assign({}, state, pick(initialState, keysToReset));
+    }
+  );
+  const resetAllActionCreatorReducer = makeActionCreatorWithReducer(
+    getActionName(reducerName, "RESET_ALL"),
+    () => {
+      return initialState;
+    }
+  );
+  const resetAll = () => resetAllActionCreatorReducer.actionCreator(null);
+  const setAll = makeActionCreatorWithReducer(
+    getActionName(reducerName, "SET_ALL"),
+    (state, newValue) => {
+      return newValue;
+    }
+  );
+  const set = makeActionCreatorWithReducer(
+    getActionName(reducerName, "SET"),
+    (state, newStateValues) => {
+      return assign({}, state, newStateValues);
+    }
+  );
+  const createdOtherActions = mapValues(otherActions, (reducer, key) => {
+    return makeActionCreatorWithReducer(
+      getActionName(reducerName, key),
+      reducer,
+      true
     );
-    const resetAllActionCreatorReducer = makeActionCreatorWithReducer(getActionName(reducerName, 'RESET_ALL'),
-        () => {
-            return initialState;
-        }
-    );
-    const resetAll = () => resetAllActionCreatorReducer.actionCreator(null);
-    const setAll = makeActionCreatorWithReducer(getActionName(reducerName, 'SET_ALL'),
-        (state, newValue) => {
-            return newValue;
-        }
-    );
-    const set = makeActionCreatorWithReducer(getActionName(reducerName, 'SET'),
-        (state, newStateValues) => {
-            return assign({},
-                state,
-                newStateValues
-            );
-        }
-    );
-    const createdOtherActions = mapValues(otherActions, (reducer, key) => {
-        return makeActionCreatorWithReducer(getActionName(reducerName, key), reducer, true);
-    })
-    return {
-        actions: assign({},
-            getCreators(assign({}, actions, { setAll, set, reset }, createdOtherActions)),
-            { resetAll }
-        ),
-        reducer: createReducer(initialState, assign({}, actions, { resetAllActionCreatorReducer, setAll, set, reset }, createdOtherActions)),
-    };
+  });
+  return {
+    actions: assign(
+      {},
+      getCreators(
+        assign({}, actions, { setAll, set, reset }, createdOtherActions)
+      ),
+      { resetAll }
+    ),
+    reducer: createReducer(
+      initialState,
+      assign(
+        {},
+        actions,
+        { resetAllActionCreatorReducer, setAll, set, reset },
+        createdOtherActions
+      )
+    ),
+  };
 }
 
 export function getActions(creators) {
-    return mapValues(creators, "actions");
+  return mapValues(creators, "actions");
 }
 
 export function makeNestedSimpleReducerSimpleActions(state) {
-    const actionsReducers = mapValues(state, (value, key) => {
-        return makeSimpleReducer(key, value.state, value.actions);
+  const actionsReducers = mapValues(state, (value, key) => {
+    return makeSimpleReducer(key, value.state, value.actions);
+  });
+  const reducers = mapValues(actionsReducers, "reducer");
+  const actions = getActions(actionsReducers);
+  const selectors = mapValues(state, (stateDepth1, stateDepth1Key) => {
+    return mapValues(stateDepth1.state, (stateDepth2Value, stateDepth2Key) => {
+      return (selectorState) => {
+        return selectorState[stateDepth1Key][stateDepth2Key];
+      };
     });
-    const reducers = mapValues(actionsReducers, 'reducer');
-    const actions = getActions(actionsReducers);
-    const selectors = mapValues(state, (stateDepth1, stateDepth1Key) => {
-        return mapValues(stateDepth1.state, (stateDepth2Value, stateDepth2Key) => {
-            return (selectorState) => {
-                return (selectorState)[stateDepth1Key][stateDepth2Key];
-            }
-        });
-    })
-    return ({
-        actions,
-        reducers,
-        selectors
-    });
+  });
+  return {
+    actions,
+    reducers,
+    selectors,
+  };
 }
 
 export function makeNestedStore(state, middleware) {
-    const { actions, reducers, selectors } = makeNestedSimpleReducerSimpleActions(state);
-    const reducer = combineReducers(reducers);
-    const store = createStore(reducer, applyMiddleware(...middleware));
-    return {
-        actions,
-        reducers,
-        selectors,
-        initialState: state,
-        reducer,
-        store
-    };
+  const { actions, reducers, selectors } = makeNestedSimpleReducerSimpleActions(
+    state
+  );
+  const reducer = combineReducers(reducers);
+  const store = createStore(reducer, applyMiddleware(...middleware));
+  return {
+    actions,
+    reducers,
+    selectors,
+    initialState: state,
+    reducer,
+    store,
+  };
 }
 
 export function mergeStateWithActions(state, actions) {
-    return {
-        state,
-        actions
-    };
+  return {
+    state,
+    actions,
+  };
 }
